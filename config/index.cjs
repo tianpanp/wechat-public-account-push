ae6ef29c6eac0a648194bb9c1dd2814afc8ab3e7
tianpanp <150404416+tianpanp@users.noreply.github.com>
2023-11-10 09:41:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wxd87d7fe507995ebb',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: 'a2c680fa149c45f75d7a3bcc83c0ca28',
 
   PROVINCE: '广东',
   CITY: '惠州',
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: 'aaa',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oJdHs6eeq5Xjw3S-IKyRkP2Zu7yI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'JGMbxrNXF4uAw34c9oAMGfb4gJ_Gwx7LiyGX7iV7Bhs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oJdHs6eeq5Xjw3S-IKyRkP2Zu7yI',
     }
   ],
 
